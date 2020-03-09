0519f6068dda5beaadb3fbc36509cfca678779ae
frankonkware <frankonkware@gmail.com>
2020-03-09 22:33:58 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express  = require('express');
+const app      = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}); //connect to database using Mongoose
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to database'));
+
+app.use(express.json());
+
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+
+app.listen(3005, () => console.log('server started') );
